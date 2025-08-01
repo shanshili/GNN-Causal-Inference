9940f2cca0b28356bd6a182b11f0a655f4133a0f
lcr508 <lcr508@123.com>
2025-08-01 11:25:19 +0800

    250801

--- Module/PCMCI_Intel.py
+++ Module/PCMCI_Intel.py
@@ -85,8 +85,8 @@ N = positions.shape[0]
 # # 设置距离阈值（根据你的数据调整）
 # distance_threshold =10.0  # 例如：只保留 _ km 以内的潜在因果关系
 
-tau_max = 55  # 可根据需要调整
-pc_alpha = 0.01  # 显著性阈值
+tau_max = 100  # 可根据需要调整
+pc_alpha = 0.001  # 显著性阈值
 
 # # 手动构造 link_assumptions（等效于 make_link_assumptions）
 # link_assumptions = {}
